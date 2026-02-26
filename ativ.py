2ebe8b9e78de8ba783b35488f709594a2c2a909c
Gabriel <aifgoiano@gmail.com>
2026-02-25 21:19:53 -0300

    Mudando Arquivo da Atividade do dia 25/02/2026

---
+++ ativ.py
@@ -0,0 +1,8 @@
+# referência e documentação: https://pythonhosted.org/scikit-fuzzy/api/api.html
+
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
+
+print
